2c7bb188972b799c2242abc6f8235165649b1c8c
Oleksandr <oleksandr.buhaiov@gmail.com>
2018-04-08 15:18:41 +0300

    hello world app

---
+++ app/src/main/java/com/osb/mobiledev/kpi/HelloScreen.java
@@ -0,0 +1,13 @@
+package com.osb.mobiledev.kpi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelloScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_screen);
+    }
+}
